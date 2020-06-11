7769b40d921a614688109c467e94a339733ae455
1234aaaa <18321018163@163.com>
2020-06-11 10:17:26 +0800

    修改报告的文件名去掉时间戳

--- run_suite.py
+++ run_suite.py
@@ -6,6 +6,7 @@ from BeautifulReport import BeautifulReport
 # 运行测试用例并生成测试报告
 import app
 
-report_name = "tpshop_report{}.html".format(time.strftime("%Y%m%d%H%M%S"))
+# report_name = "tpshop_report{}.html".format(time.strftime("%Y%m%d%H%M%S"))
+report_name = "tpshop_report.html"
 suite = unittest.TestLoader().discover(app.BASE_DIR + "/script", pattern="test*.py")
 BeautifulReport(suite).report(filename=report_name, description="TpShop报告", log_path=app.BASE_DIR + "/report")
